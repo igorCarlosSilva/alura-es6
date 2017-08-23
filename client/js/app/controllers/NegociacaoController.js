d0e2d38b97898a638975156447487ca5bfc6130b
Igor Carlos da Silva <igor.silva@corebiz.com.br>
2017-08-23 18:29:30 -0300

    terminando modulo 4

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -9,18 +9,23 @@ class NegociacaoController {
 
     }
 
-    adiciona(event){
+    adiciona(event) {
         event.preventDefault();
 
+        let data = new Date(...
+            this._inputData.value
+                .split('-')
+                .map((item, index) =>  item - index % 2)
+        );
+
+
         let negociacao = new Negociacao(
-            this._inputData.value,
+            data,
             this._inputQuantidade.value,
             this._inputValor.value
         );
 
         console.log(negociacao);
-
-        //adicionar a negociacao em uma lista
     }
 
 }
